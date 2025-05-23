5668ad1e2e71e59b85ccc6fc8018871276b1bd62
yogesh-taikisha <yogeshkumar.taikisha@gmail.com>
2025-05-23 06:20:14 +0000

    binding and inline style

---
+++ ch15/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<!-- Bindging Inline Style -->
+<template>
+ <h1 style="color: black; background-color: beige;"> Hello World</h1>
+ <h1 v-bind:style="{color: 'black', backgroundColor: 'beige'}"> Hello World</h1>
+    <!-- if the colors are not written in quotes then those are treated as variables that needs to be defined on instance -->
+    <!-- the background color is written in camelcase unlike css way of writing it -->
+ <h1 v-bind:style="{color: 'black', 'background-color': 'yellow'}"> Hello World</h1>
+
+</template>
+
+<script setup>
+
+</script>
